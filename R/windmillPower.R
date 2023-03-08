547ed02807b35c65352ceae74c65c1809ce0cc89
chucktheivy <charliewilson@ucsb.edu>
2023-03-08 10:45:05 -0800

    wrote out parameters

---
+++ R/windmillPower.R
@@ -0,0 +1,12 @@
+#' Compute windmill power
+#' @param p is air density
+#' @param A is rotar swept area m^2
+#' @param Cp is power coefficient
+#' @param V is wind velocity m/s
+#' @param Ng is generator efficiency
+#' @param Nb is gearbox efficiency
+#' @param price is $ per kWh
+
+windmill_power = function(p, A, Cp, V, Ng, Nb, price) {
+  
+}
