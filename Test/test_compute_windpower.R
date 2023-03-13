aa0c6c94d2c5e9d8d4db0b941ab034be16affe0f
chucktheivy <charliewilson@ucsb.edu>
2023-03-13 09:55:31 -0700

    fixed test

--- Test/test_compute_windpower.R
+++ Test/test_compute_windpower.R
@@ -1,6 +1,5 @@
 #trying to test that a swpet area (A) of zero will always generate zero
 
 test_that("windmill_power_works", {
-  
-  expect_equal(windmill_power(A=0, V=25, Ng=0.2), 0)
+  expect_equal(windmill_power(A=20,V=0,hidePrice = TRUE, hidePower = FALSE), 0)
 })
--- Test/windmill_test.R
+++ Test/windmill_test.R
@@ -1,3 +1,6 @@
+library(here)
+source(here("windmill","R","windmill_power.R"))
+
 test_that("windmill_power_works", {
   expect_equal(windmill_power(A=20,V=0,hidePrice = TRUE, hidePower = FALSE), 0)
 })
